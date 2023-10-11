ee02fbe75fd53ca7eded6ef57d9ab02c20cd7629
mitchelloharawild <mitch.ohara-wild@monash.edu>
2023-10-12 10:03:42 +1100

    Add use_course() zip and set-up instructions for exercises

--- zip.R
+++ zip.R
@@ -4,9 +4,10 @@ zip(
   flags = "-j"
 )
 
-# zip(
-#   "labs.zip",
-#   list.files("_site/labs/", pattern = "\\.r$", full.names = TRUE),
-#   flags = "-j"
-# )
-
+setwd("exercises/")
+unlink("../exercises.zip")
+zip(
+  "../exercises.zip",
+  list.files(".", recursive = TRUE, full.names = TRUE)
+)
+setwd("../")
