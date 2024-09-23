51f83fa3617dbd173ea19ee8af7d3b233c4d7258
mitchelloharawild <mitch.ohara-wild@monash.edu>
2024-09-23 20:43:17 +1000

    Add day 1 session 2 exercises

--- demo/day1.R
+++ demo/day1.R
@@ -21,3 +21,42 @@ prison |>
   as_tsibble(index = quarter, key = c(state, gender, legal, indigenous))
 
 PBS
+
+
+## Session 2
+
+ansett %>%
+  filter(Airports=="MEL-SYD", Class=="Economy") %>%
+  autoplot(Passengers)
+
+ansett %>%
+  filter(Airports=="MEL-SYD") %>%
+  autoplot(Passengers)
+
+PBS %>%
+  filter(ATC2 == "A10") %>%
+  summarise(Cost = sum(Cost)/1e6) %>%
+  autoplot(Cost) +
+  ylab("$ million") + xlab("Year") +
+  ggtitle("Antidiabetic drug sales")
+
+library(fpp3)
+holidays <- tourism |>
+  filter(Purpose == "Holiday") |>
+  group_by(State) |>
+  summarise(Trips = sum(Trips))
+
+## gg_season(), gg_subseries()
+holidays |> 
+  autoplot(Trips)
+
+holidays |> 
+  gg_season(Trips)
+
+holidays |> 
+  gg_subseries(Trips)
+
+
+# No seasonality in annual data
+global_economy |> 
+  gg_season(GDP)
