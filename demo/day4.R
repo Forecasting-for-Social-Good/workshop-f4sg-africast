03f8d23deda0bc72b536619e8e50b95a38ff0545
mitchelloharawild <mitch.ohara-wild@monash.edu>
2024-09-27 18:47:40 +1000

    Built site for gh-pages

--- demo/day4.R
+++ demo/day4.R
@@ -1,143 +1,63 @@
 library(fpp3)
-# Recap from Day 3
-aus_tourism_purpose <- tourism|> group_by(Purpose) |> 
-  summarise(Trips =sum(Trips))
-
-fit <- aus_tourism_purpose |> 
-  model(average = MEAN(Trips),
-        naive = NAIVE(Trips),
-        seasonal_naive = SNAIVE(Trips),
-        Regression = TSLM(Trips ~ trend() + season()))
-
-fit |> glance()
-fit |> tidy()
-fit |> select(Regression) |> filter(Purpose == "Holiday") |> report()
-
-fcst <- fit |> forecast(h = "2 years")
-fcst <- fit |> forecast(h = 24)
-
-fcst |> autoplot(level = NULL)
-
-fcst |> hilo(level = 90) |> unpack_hilo("90%")
-
-library(ggdist)
-fcst |> ggplot(aes(x = Quarter, ydist =Trips))+
-  stat_halfeye()+
-  facet_wrap(vars(Purpose))+
-  labs(x = "Quarter", y="Trips", title = " Forecast distributions of trips")+
-  ggthemes::theme_clean()
-
-#------------------------------- ETS---------------------------------------------------
-
-fit <- global_economy |> filter(Country == "Australia") |> 
-  model(ses = ETS(Exports ~ error("A") + trend("N") + season("N")))
-
-fit |> glance()
-fit |> tidy()
-fit |> report()
-
-fit |> forecast(h=10) |> autoplot()
-
-##
-
-aus_economy <- global_economy |> filter(Country == "Australia") |>
-  mutate(Pop = Population / 1e6)
-autoplot(aus_economy)
-fit <- aus_economy |> model(ETS(Pop))
-
-report(fit)
-fit |> components() |> autoplot()
-
-fit |> forecast(h=10) |> autoplot(aus_economy)
-
-## Holidays
-holiday <- tourism |> filter(Purpose == "Holiday")
-
-fit <- holiday |> model(ets = ETS(Trips))
-
-fit |> filter(Region == "Snowy Mountains") |> report()
-
-fit |> filter(Region == "Snowy Mountains") |> components() |> autoplot()
-
-##
-cafe_vic <- tsibbledata::aus_retail |> 
-  filter(State == "Victoria",
-         Industry == "Cafes, restaurants and catering services"
-         ) |> select(Month,Turnover)
-
-#how to forecast with transformations
-cafe_vic |> autoplot(box_cox(Turnover, lambda = .2))
-
-#fit <- cafe_vic |> model(ets = ETS(Turnover))
-
-fit <- cafe_vic |> model(ets = ETS(box_cox(Turnover, lambda = .2)))
-fct <- fit |> forecast(h = 12)
-fct |> autoplot(filter_index(cafe_vic, "2016 Jan" ~ .))
-
-# Generate function to produce forecasts using bootstrapping
-
-simulation <- fit |> generate(h= 12, times = 1000, bootstrap = TRUE)
-
-cafe_vic |> filter_index("2016 Jan" ~ .) |> 
-  ggplot(aes(x = Month))+
-  geom_line(aes(y =Turnover))+
-  geom_line(aes( y = .sim, colour = as.factor(.rep)), data = simulation)+
-  guides(col = FALSE)
-
-fct_boot <- fit |> forecast(h= 12, bootstrap = TRUE)
-
-fct_boot |> hilo(level = 90)
-
-fct_boot |> 
-ggplot(aes(x = Month, ydist =Turnover))+
-  stat_halfeye()+
-  autolayer(cafe_vic |> filter_index("2018 Jan" ~ .))
-
-## ------------------------ARIMA -----------------------------------
-egy_economy <- global_economy |> filter(Code == "EGY") 
-egy_economy |> autoplot(Exports)
-
-fit <- egy_economy |> model(arima = ARIMA(Exports))
-
-report(fit)
-
-fit |> forecast(h=5) |> autoplot(egy_economy)
-
-###
-
-h02 <- PBS |> filter(ATC2 == "H02") |> 
-  summarise(Cost = sum(Cost)/ 1e6)
-h02 |> autoplot(Cost)
-
-h02 |> autoplot(log(Cost) |> difference(12) |> 
-                  difference(1))
-
-#stationairy
-h02 |> features(Cost, unitroot_kpss)
-
-h02 |> features(Cost, unitroot_nsdiffs)
-
-##
-h02 |> ACF(Cost) |> autoplot()
-h02 |> PACF(Cost) |> autoplot()
-
-fit <- h02 |> model(arima= ARIMA(log(Cost)))
-report(fit)
-fit|> 
-  forecast(h= "3 years")
 
 
-## ------------------------ ensemble ----------------------------------
-aus_tourism <- tourism |> index_by(Quarter) |> summarise(Trips = sum(Trips))
+aus_beer <- aus_production |> select(Beer)
 
-fit <- aus_tourism |> model(
-  snaive = SNAIVE(Trips),
-  ets= ETS(Trips),
-  arima = ARIMA(Trips),
-  regression = TSLM(Trips ~ trend() + season()),
-) |> 
-  mutate(combination = (regression+snaive+ets+arima)/4)
+# ETS with one series
+fit_beer_ets <- aus_beer |> 
+  model(exponential_smmothing = ETS(Beer),
+        exponential_smmothing1 = ETS(Beer ~ error("A") + trend("N", alpha = 0.1) + season("N")),
+        snaive = SNAIVE(Beer),
+        average = MEAN(Beer))
 
-fcst <- fit |> forecast(h=12) 
+report(fit_beer)
+glance(fit_beer)
+tidy(fit_beer)
+components(fit_beer)
 
-fcst|> autoplot(aus_tourism)
+fcst_beer_ets <- fit_beer |> forecast(h= 12)
+
+# ETS with many series series
+tourism_fit <- tourism |> 
+  model(exponential_smmothing = ETS(Trips),
+        exponential_smmothing1 = ETS(Trips ~ error("A") + trend("N", alpha = 0.1) + season("N")),
+        snaive = SNAIVE(Trips),
+        average = MEAN(Trips))
+
+fcst_tourism_ets <- tourism_fit |> forecast(h= 12)
+
+# ARIMA
+fit_beer_arima <- aus_beer |> model(arima = ARIMA(Beer),
+                                    arima1 = ARIMA(Beer ~ pdq(1,1,0)+PDQ(0,0,0)))
+
+
+report(fit_beer_arima)
+glance(fit_beer_arima)
+tidy(fit_beer_arima)
+
+fcst_beer_ets <- fit_beer_arima |> 
+  forecast(h= 12)
+
+# Look at ACF and PACF to decide p,q, P,Q
+aus_beer |> ACF(Beer) |> autoplot()
+aus_beer |> PACF(Beer) |> autoplot()
+
+
+
+# include all models covered so far
+
+fit_beer_ets <- aus_beer |> 
+  model(exponential_smmothing = ETS(Beer),
+        snaive = SNAIVE(Beer),
+        average = MEAN(Beer),
+        naive = NAIVE(Beer), 
+        arima = ARIMA(Beer),
+        regression = TSLM(Beer))
+
+fcst_beer <- fit_beer_ets |> forecast(h=12)
+report(fit_beer_ets)
+glance(fit_beer_ets)
+tidy(fit_beer_ets)
+
+
+
