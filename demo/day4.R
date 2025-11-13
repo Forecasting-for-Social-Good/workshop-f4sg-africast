bbfac361c051d63ab8bb1ded04e0d133582c6fc6
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2025-11-13 22:21:32 +1100

    Built site for gh-pages

--- demo/day4.R
+++ demo/day4.R
@@ -1,63 +1,116 @@
-library(fpp3)
+library(fpp3) # for forecast and tsibble)
+library(ggdist)# for visualizing distributions
+#
+
+tourism_holiday <- tourism |> filter(Purpose == "Holiday") |> summarise(Trips = sum(Trips)) #
+# Fit a seasonal naive model to the holiday tourism data
+fit <- tourism_holiday |> model(snaive = SNAIVE(Trips))
+# Generate forecasts for the next 6 quarters
+fcst <- fit |> forecast(h=6)
+# Fit multiple models to the holiday tourism data
+fit_all <- tourism |> model(snaive = SNAIVE(Trips),
+                            naive = NAIVE(Trips),
+                            average = MEAN(Trips),
+                            regression = TSLM(Trips ~ trend() + season())
+)
+# Generate forecasts for the next 6 quarters from all models
+fcst_all <- fit_all |> forecast(h=6)
+# Plot the forecast distribution along with fitted values and actual data (this plot is not suitabel for many time series, you need to selct which series befoe using the plot)
+ggplot(data = fcst, aes(x = Quarter, ydist = Trips))+
+  ggdist::stat_halfeye(alpha = .4) +
+  geom_line(aes(y = .mean, colour = "Point Forecast"))+
+  geom_line(aes(y = .fitted, colour = "Fitted"), data = augment(fit))+
+  geom_point(aes(y = .fitted, colour = "Fitted"), data = augment(fit))+
+  geom_line(aes(y = Trips, colour = "Data"), data = tourism_holiday)+
+  geom_point(aes(y = Trips, colour = "Data"), data = tourism_holiday)+
+  scale_color_manual(name = NULL,
+                     breaks = c("Fitted", "Data", "Point Forecast"),
+                     values = c("Fitted" = "#E69F00", "Data" = "#0072B2", "Point Forecast" = "#000000"))
 
 
-aus_beer <- aus_production |> select(Beer)
-
-# ETS with one series
-fit_beer_ets <- aus_beer |> 
-  model(exponential_smmothing = ETS(Beer),
-        exponential_smmothing1 = ETS(Beer ~ error("A") + trend("N", alpha = 0.1) + season("N")),
-        snaive = SNAIVE(Beer),
-        average = MEAN(Beer))
-
-report(fit_beer)
-glance(fit_beer)
-tidy(fit_beer)
-components(fit_beer)
-
-fcst_beer_ets <- fit_beer |> forecast(h= 12)
-
-# ETS with many series series
-tourism_fit <- tourism |> 
-  model(exponential_smmothing = ETS(Trips),
-        exponential_smmothing1 = ETS(Trips ~ error("A") + trend("N", alpha = 0.1) + season("N")),
-        snaive = SNAIVE(Trips),
-        average = MEAN(Trips))
-
-fcst_tourism_ets <- tourism_fit |> forecast(h= 12)
-
-# ARIMA
-fit_beer_arima <- aus_beer |> model(arima = ARIMA(Beer),
-                                    arima1 = ARIMA(Beer ~ pdq(1,1,0)+PDQ(0,0,0)))
+# Fit an ETS model to the holiday tourism data
+fit_ets <- tourism_holiday |> model(exponential_smoothing = ETS(Trips))
+# Fit different models of ETS to the holiday tourism data, with specific components and smoothing parameters
+fit_ets <- tourism_holiday |> model(exponential_smoothing = ETS(Trips), # automatic ETS
+                                    additive_damped = ETS(Trips ~ error("A") + trend("Ad") + season("A")),#
+                                    multiplicative = ETS(Trips ~ error("M") + trend("A") + season("M")), #
+                                    multiplicative_damped = ETS(Trips ~ error("M") + trend("Md") + season("M")),#
+                                    additive = ETS(Trips ~ error("A") + trend("A") + season("A")),#
+                                    simple_ets = ETS(Trips ~ error("A") + trend("N") + season("N")),#
+                                    simple_ets_alpha = ETS(Trips ~ error("A") + trend("N", alpha=0.1) + season("N")),#
+                                    simple_ets_alpha_beta = ETS(Trips ~ error("A") + trend("A", alpha=0.1, beta=0.01) + season("N")),#
+                                    simple_ets_beta = ETS(Trips ~ error("A") + trend("A", beta=0.01) + season("N")),#
+                                    simple_ets_alpha_beta_gamma = ETS(Trips ~ error("A") + trend("A") + season("A", gamma=0.1))#
+)
 
 
-report(fit_beer_arima)
-glance(fit_beer_arima)
-tidy(fit_beer_arima)
-
-fcst_beer_ets <- fit_beer_arima |> 
-  forecast(h= 12)
-
-# Look at ACF and PACF to decide p,q, P,Q
-aus_beer |> ACF(Beer) |> autoplot()
-aus_beer |> PACF(Beer) |> autoplot()
-
-
-
-# include all models covered so far
-
-fit_beer_ets <- aus_beer |> 
-  model(exponential_smmothing = ETS(Beer),
-        snaive = SNAIVE(Beer),
-        average = MEAN(Beer),
-        naive = NAIVE(Beer), 
-        arima = ARIMA(Beer),
-        regression = TSLM(Beer))
-
-fcst_beer <- fit_beer_ets |> forecast(h=12)
-report(fit_beer_ets)
-glance(fit_beer_ets)
-tidy(fit_beer_ets)
+# Generate forecasts for the next 6 quarters from the ETS model
+fcst_ets <- fit_ets |> forecast(h=6)
+# Extract and view the components of the ETS model
+fit_ets |> components()
+# Generate a report of the ETS model
+fit_ets |> report()
+# Tidy the ETS model output
+fit_ets |> tidy()
+# Glance at the ETS model summary
+fit_ets |> glance()
+# Augment the ETS model with fitted values
+fit_ets |> augment()
 
 
+## ARIMA (session 2)
+global_economy |>
+  filter(Country == "Australia") |>
+  autoplot(GDP)
 
+# Stationary time series
+# 1. Constant variance (transform the time series log/box_cox)
+global_economy |>
+  filter(Country == "Australia") |>
+  autoplot(log(GDP))
+
+# 2. Constant mean (no trend, if trended we need to difference the data)
+global_economy |>
+  filter(Country == "Australia") |>
+  autoplot(difference(log(GDP)))
+
+fit_arima <- global_economy |>
+  filter(Country == "Australia") |>
+  model(arima = ARIMA(log(GDP), trace = TRUE))
+
+fit_arima <- global_economy |>
+  filter(Country == "Australia") |>
+  model(arima = ARIMA(log(GDP), stepwise = FALSE, trace = TRUE))
+
+fit_arima |>
+  forecast(h = "10 years") |>
+  autoplot(global_economy)
+
+
+tourism_holiday |>
+  autoplot(Trips)
+
+tourism_holiday |>
+  model(ETS(Trips))
+
+tourism_holiday |>
+  autoplot(log(Trips))
+
+fit <- tourism_holiday |>
+  model(
+    arima = ARIMA(log(Trips)),
+    ets = ETS(Trips)
+  )
+
+fit |>
+  forecast(h = "3 years") |>
+  autoplot(tourism_holiday)
+
+fit_ensemble <- fit |>
+  mutate(
+    ensemble = (arima+ets)/2
+  )
+
+fit_ensemble |>
+  forecast(h = "3 years") |>
+  autoplot(tourism_holiday |> tail(12))
